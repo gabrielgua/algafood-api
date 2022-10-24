8b0999b0fb595226b0a09d4a19cd0b57bb5d4857
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2022-10-24 14:52:29 -0300

    customizando serialização do json para as paginações

---
+++ src/main/java/com/gabriel/algafood/core/jackson/PageJsonSerializer.java
@@ -0,0 +1,31 @@
+package com.gabriel.algafood.core.jackson;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.data.domain.Page;
+
+import java.io.IOException;
+
+@JsonComponent
+public class PageJsonSerializer extends JsonSerializer<Page<?>> {
+
+    @Override
+    public void serialize(Page<?> value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+
+        gen.writeStartObject();
+
+        gen.writeObjectField("content", value.getContent());
+        gen.writeNumberField("size", value.getSize());
+        gen.writeNumberField("totalPages", value.getTotalPages());
+        gen.writeNumberField("totalElements", value.getTotalElements());
+        gen.writeNumberField("page", value.getNumber());
+
+
+
+
+
+        gen.writeEndObject();
+    }
+}
