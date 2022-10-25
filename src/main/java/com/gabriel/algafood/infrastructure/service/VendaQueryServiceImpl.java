307733ae15bc8a87f7775a37c29f18e9355269b8
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2022-10-25 18:38:10 -0300

    filtros para vendas diarias implementados

--- src/main/java/com/gabriel/algafood/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/com/gabriel/algafood/infrastructure/service/VendaQueryServiceImpl.java
@@ -8,9 +8,13 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.time.LocalDate;
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import static com.gabriel.algafood.domain.model.StatusPedido.CONFIRMADO;
+import static com.gabriel.algafood.domain.model.StatusPedido.ENTREGUE;
 
 @Repository
 public class VendaQueryServiceImpl implements VendaQueryService {
@@ -30,9 +34,26 @@ public class VendaQueryServiceImpl implements VendaQueryService {
                 functionDateDataCriacao,
                 builder.count(root.get("id")),
                 builder.sum(root.get("valorTotal"))
-                );
+        );
+
+        List<Predicate> predicates = new ArrayList<Predicate>();
+
+        if (filtro.getRestauranteId() != null) {
+            predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauranteId()));
+        }
+
+        if (filtro.getDataCriacaoInicio() != null) {
+            predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoInicio()));
+        }
+
+        if (filtro.getDataCriacaoFim() != null) {
+            predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoFim()));
+        }
+
+        predicates.add(root.get("status").in(CONFIRMADO, ENTREGUE));
 
         query.select(selection);
+        query.where(predicates.toArray(new Predicate[0]));
         query.groupBy(functionDateDataCriacao);
 
         return manager.createQuery(query).getResultList();
