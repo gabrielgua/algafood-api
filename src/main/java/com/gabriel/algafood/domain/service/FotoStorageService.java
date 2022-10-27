d7461da2c936f0fa34b52b8b13c1e0a46e972969
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2022-10-27 18:39:56 -0300

    serviço de armazenagem de fotos no disco local implementado

---
+++ src/main/java/com/gabriel/algafood/domain/service/FotoStorageService.java
@@ -0,0 +1,18 @@
+package com.gabriel.algafood.domain.service;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.io.InputStream;
+
+public interface FotoStorageService {
+
+    void armazenar(NovaFoto novaFoto);
+
+    @Getter
+    @Builder
+    class NovaFoto {
+        private String nomeArquivo;
+        private InputStream inputStream;
+    }
+}
---
+++ src/main/java/com/gabriel/algafood/infrastructure/storage/LocalFotoStorageServiceImpl.java
@@ -0,0 +1,34 @@
+package com.gabriel.algafood.infrastructure.storage;
+
+import com.gabriel.algafood.domain.exception.StorageException;
+import com.gabriel.algafood.domain.service.FotoStorageService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Service
+public class LocalFotoStorageServiceImpl implements FotoStorageService {
+
+    @Value("${algafood.storage.local.diretorio-fotos}")
+    private Path diretorioFoto;
+
+    @Override
+    public void armazenar(NovaFoto novaFoto) {
+        Path arquivoPath = getArquivoPath(novaFoto.getNomeArquivo());
+
+        try {
+            FileCopyUtils.copy(novaFoto.getInputStream(), Files.newOutputStream(arquivoPath));
+
+
+        } catch (Exception ex) {
+            throw new StorageException("Não foi possível armazenar o arquivo", ex);
+        }
+    }
+
+    private Path getArquivoPath(String nomeArquivo) {
+        return diretorioFoto.resolve(Path.of(nomeArquivo));
+    }
+}
