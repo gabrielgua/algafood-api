02e504a7e1fca230976184ed68c47d0896d9a605
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2022-11-01 18:48:34 -0300

    definindo bean do client da amazon s3 e configurando credenciais

--- src/main/java/com/gabriel/algafood/infrastructure/storage/S3FotoStorageService.java
+++ src/main/java/com/gabriel/algafood/infrastructure/storage/S3FotoStorageService.java
@@ -1,6 +1,8 @@
 package com.gabriel.algafood.infrastructure.storage;
 
+import com.amazonaws.services.s3.AmazonS3;
 import com.gabriel.algafood.domain.service.FotoStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
@@ -8,6 +10,8 @@ import java.io.InputStream;
 @Service
 public class S3FotoStorageService implements FotoStorageService {
 
+    @Autowired
+    private AmazonS3 amazonS3;
 
     @Override
     public void armazenar(NovaFoto novaFoto) {
