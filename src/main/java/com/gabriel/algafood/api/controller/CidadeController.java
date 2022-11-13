67694183681e4e11453713b20aab9cf39ab5aa17
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2022-11-13 02:45:59 -0300

    começando a documentação de cidades e estados

--- src/main/java/com/gabriel/algafood/api/controller/CidadeController.java
+++ src/main/java/com/gabriel/algafood/api/controller/CidadeController.java
@@ -7,6 +7,9 @@ import com.gabriel.algafood.domain.exception.EstadoNaoEncontradoException;
 import com.gabriel.algafood.domain.exception.NegocioException;
 import com.gabriel.algafood.domain.model.Cidade;
 import com.gabriel.algafood.domain.service.CidadeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Api(tags = "Cidades")
 @RestController
 @AllArgsConstructor
 @RequestMapping("cidades")
@@ -23,19 +27,26 @@ public class CidadeController {
     private CidadeService service;
     private CidadeAssembler assembler;
 
+    @ApiOperation("Lista as cidades")
     @GetMapping
     public List<CidadeModel> listar() {
         return assembler.toCollectionModel(service.listar());
     }
 
+    @ApiOperation("Busca uma cidade por ID")
     @GetMapping("/{id}")
-    public CidadeModel buscarPorId(@PathVariable Long id) {
+    public CidadeModel buscarPorId(
+            @ApiParam(value = "ID de uma cidade", example = "1")
+            @PathVariable Long id) {
         return assembler.toModel(service.buscarPorId(id));
     }
 
+    @ApiOperation("Cadastra uma cidade")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CidadeModel salvar(@RequestBody @Valid CidadeRequest request) {
+    public CidadeModel salvar(
+            @ApiParam(name = "Corpo", value = "Representação de uma nova cidade")
+            @RequestBody @Valid CidadeRequest request) {
         try {
             Cidade cidade = assembler.toEntity(request);
             return assembler.toModel(service.salvar(cidade));
@@ -44,8 +55,13 @@ public class CidadeController {
         }
     }
 
+    @ApiOperation("Atualiza uma cidade por ID")
     @PutMapping("/{id}")
-    public CidadeModel editar(@RequestBody @Valid CidadeRequest request, @PathVariable Long id) {
+    public CidadeModel editar(
+            @ApiParam(name = "Corpo", value = "Representação de uma cidade com os novos dados")
+            @RequestBody @Valid CidadeRequest request,
+            @ApiParam(value = "ID de uma cidade", example = "2")
+            @PathVariable Long id) {
         try {
             Cidade cidadeAtual = service.buscarPorId(id);
             assembler.copyToEntity(request, cidadeAtual);
@@ -55,8 +71,11 @@ public class CidadeController {
         }
     }
 
+    @ApiOperation("Remove uma cidade por ID")
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> remover(@PathVariable Long id) {
+    public ResponseEntity<Void> remover(
+            @ApiParam("ID de uma cidade")
+            @PathVariable Long id) {
         Cidade cidade = service.buscarPorId(id);
         service.remover(cidade.getId());
         return ResponseEntity.noContent().build();
