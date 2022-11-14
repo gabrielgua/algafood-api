c4bf10f86ea344fbcb2a718acf286b53e1e53d01
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2022-11-14 20:43:32 -0300

    implementação da documentação de permissões e grupos

--- src/main/java/com/gabriel/algafood/api/controller/GrupoPermissaoController.java
+++ src/main/java/com/gabriel/algafood/api/controller/GrupoPermissaoController.java
@@ -2,6 +2,7 @@ package com.gabriel.algafood.api.controller;
 
 import com.gabriel.algafood.api.assembler.PermissaoAssembler;
 import com.gabriel.algafood.api.model.PermissaoModel;
+import com.gabriel.algafood.api.openapi.controller.GrupoPermissaoControllerOpenApi;
 import com.gabriel.algafood.domain.model.Grupo;
 import com.gabriel.algafood.domain.service.GrupoService;
 import lombok.AllArgsConstructor;
@@ -12,7 +13,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/grupos/{grupoId}/permissoes")
-public class GrupoPermissaoController {
+public class GrupoPermissaoController implements GrupoPermissaoControllerOpenApi {
 
     private GrupoService grupoService;
     private PermissaoAssembler permissaoAssembler;
