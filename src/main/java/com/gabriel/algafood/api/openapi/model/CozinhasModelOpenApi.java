8472a94841848510d322e555233c4210ce971a51
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2022-11-21 19:07:04 -0300

    corrigindo documentação de cozinhas

--- src/main/java/com/gabriel/algafood/api/openapi/model/CozinhasModelOpenApi.java
+++ src/main/java/com/gabriel/algafood/api/openapi/model/CozinhasModelOpenApi.java
@@ -2,8 +2,25 @@ package com.gabriel.algafood.api.openapi.model;
 
 import com.gabriel.algafood.api.model.CozinhaModel;
 import io.swagger.annotations.ApiModel;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.hateoas.Links;
+
+import java.util.List;
 
 @ApiModel("CozinhasModel")
-public class CozinhasModelOpenApi extends PagedModelOpenApi<CozinhaModel> {
+@Setter
+@Getter
+public class CozinhasModelOpenApi {
+
+    private CozinhasEmbeddedModelOpenApi _embedded;
+    private Links _links;
+    private PageModelOpenApi page;
+
+    @Getter
+    @Setter
+    private class CozinhasEmbeddedModelOpenApi {
+        private List<CozinhaModel> cozinhas;
+    }
 
 }
