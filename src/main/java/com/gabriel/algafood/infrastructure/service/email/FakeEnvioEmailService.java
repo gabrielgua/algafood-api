b2218b8ff7fa8c16ea3a2b7f57af149bf639e6b0
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2023-03-02 20:44:06 -0300

    preparando projeto para deploy na aws

--- src/main/java/com/gabriel/algafood/infrastructure/service/email/FakeEnvioEmailService.java
+++ src/main/java/com/gabriel/algafood/infrastructure/service/email/FakeEnvioEmailService.java
@@ -2,13 +2,17 @@ package com.gabriel.algafood.infrastructure.service.email;
 
 import com.gabriel.algafood.domain.service.EnvioEmailService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Slf4j
-public class FakeEnvioEmailService extends SmtpEnvioEmailService {
+public class FakeEnvioEmailService implements EnvioEmailService {
+
+    @Autowired
+    private ProcessadorEmailTemplate template;
 
     @Override
     public void enviar(Mensagem mensagem) {
-        String corpo = processarTemplate(mensagem);
+        String corpo = template.processarTemplate(mensagem);
 
         log.info("[FAKE E-MAIL] Para: {}\n{}", mensagem.getDestinatarios(), corpo);
     }
